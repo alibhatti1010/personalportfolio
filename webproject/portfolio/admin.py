c44fb40ed00adaeef08cc256844039cbb1b4eb91
alibhatti1010 <128999591+alibhatti1010@users.noreply.github.com>
2024-06-30 01:08:03 +0500

    Delete webproject directory

--- webproject/portfolio/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Education, Experience, Skill, Product, Contact, OwnerInfo
-
-admin.site.register(Education)
-admin.site.register(Experience)
-admin.site.register(Skill)
-admin.site.register(Product)
-admin.site.register(Contact)
-admin.site.register(OwnerInfo)
