b95a125a795da0f74ff30b4190fa447ee08186cb
alibhatti1010 <128999591+alibhatti1010@users.noreply.github.com>
2024-06-30 01:10:29 +0500

    Add files via upload

---
+++ webproject/portfolio/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Education, Experience, Skill, Product, Contact, OwnerInfo
+
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Skill)
+admin.site.register(Product)
+admin.site.register(Contact)
+admin.site.register(OwnerInfo)
